78faaf17161ee360bd38c61858515f8e215b2d6d
denspiro <spiridonovdenis83@gmail.com>
2023-04-24 08:36:20 +0300

    TreeSetter fix

--- init.lua
+++ init.lua
@@ -352,6 +352,40 @@ return require('packer').startup(function(use)
     },
   }
 
+  local null_ls = require("null-ls")
+  null_ls.setup({
+    sources = {
+      null_ls.builtins.formatting.prettierd,
+    },
+  })
+
+  local lsp_formatting = function(bufnr)
+      vim.lsp.buf.format({
+          filter = function(client)
+              -- apply whatever logic you want (in this example, we'll only use null-ls)
+              return client.name == "null-ls"
+          end,
+          bufnr = bufnr,
+      })
+  end
+
+  -- if you want to set up formatting on save, you can use this as a callback
+  --local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
+
+  -- add to your shared on_attach callback
+  local on_attach = function(client, bufnr)
+      if client.supports_method("textDocument/formatting") then
+          vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+          vim.api.nvim_create_autocmd("BufWritePre", {
+              group = augroup,
+              buffer = bufnr,
+              callback = function()
+                  lsp_formatting(bufnr)
+              end,
+          })
+      end
+  end
+
   -- Setting up Mason for LSP, DAP etc.
   require("mason").setup()
   require("mason-lspconfig").setup({
@@ -497,13 +531,6 @@ return require('packer').startup(function(use)
     }
   })
 
-  local null_ls = require("null-ls")
-  null_ls.setup({
-    sources = {
-      null_ls.builtins.formatting.prettierd,
-    },
-  })
-
   -- Add additional capabilities supported by nvim-cmp
   local capabilities = vim.lsp.protocol.make_client_capabilities()
   capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
@@ -631,6 +658,9 @@ return require('packer').startup(function(use)
 
   -- Reload buffer
   keybind('n', '<C-a>', ':windo e!<CR>')
+
+  -- Formatting
+  keybind('v', '<leader>f', vim.lsp.buf.format, opts)
 
   -- Move lines up and down
   keybind('n', '<C-j>', ':m .+1<CR>==', opts)
