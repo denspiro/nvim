86763da5318a7491883bde268a9cbf2a4712bda9
Denis Sinko <sindeko@proton.me>
2024-05-01 15:57:10 +0300

    010524 chore: add neogit and autocompletion plugins

--- lua/plugins/packer.lua
+++ lua/plugins/packer.lua
@@ -2,25 +2,34 @@
 
 return require("packer").startup(function()
   -- Comments
-  use("numToStr/Comment.nvim") -- Enable comments
+  use("numToStr/Comment.nvim")    -- Enable comments
   use("folke/todo-comments.nvim") -- Todo comments
 
   -- Completion
-  use("hrsh7th/nvim-cmp") -- Completion framework
-  use("hrsh7th/cmp-nvim-lua") -- nvim-cmp source for neovim Lua API.
-  use("hrsh7th/cmp-nvim-lsp") -- LSP completion source for nvim-cmp
+  use("hrsh7th/nvim-cmp")         -- Completion framework
+  use("hrsh7th/cmp-nvim-lua")     -- nvim-cmp source for neovim Lua API.
+  use("hrsh7th/cmp-nvim-lsp")     -- LSP completion source for nvim-cmp
   use("hrsh7th/cmp-nvim-lsp-signature-help")
-  use("hrsh7th/cmp-vsnip") -- Snippet completion source for nvim-cmp
-  use("hrsh7th/cmp-path") -- Useful completion sources
-  use("hrsh7th/cmp-buffer") -- Useful completion sources
-  use("L3MON4D3/LuaSnip") -- snippets for completion
+  use("hrsh7th/cmp-vsnip")        -- Snippet completion source for nvim-cmp
+  use("hrsh7th/cmp-path")         -- Useful completion sources
+  use("hrsh7th/cmp-buffer")       -- Useful completion sources
+  use("L3MON4D3/LuaSnip")         -- snippets for completion
   use("saadparwaiz1/cmp_luasnip") -- snippets for completion'
   use({
-    "windwp/nvim-autopairs", -- Autocomplete (), {}, []
+    "kylechui/nvim-surround",
+    tag = "*", -- Use for stability; omit to use `main` branch for the latest features
     config = function()
-      require("nvim-autopairs").setup({})
+      require("nvim-surround").setup({})
     end,
-  })
+  }) -- Surround selections
+  use({
+    "altermo/ultimate-autopair.nvim",
+    event = { "InsertEnter", "CmdlineEnter" },
+    branch = "v0.6", --recommended as each new version will have breaking changes
+    config = function()
+      require("ultimate-autopair").setup({})
+    end,
+  }) -- Autopair on insert ( { "" } )
 
   -- Debugger
   use("mfussenegger/nvim-dap")
@@ -28,35 +37,51 @@ return require("packer").startup(function()
 
   -- Folding
   use({ "kevinhwang91/nvim-ufo", requires = "kevinhwang91/promise-async" }) -- good folding
-  use("lukas-reineke/indent-blankline.nvim") -- ident lines
+  use("lukas-reineke/indent-blankline.nvim")                                -- ident lines
 
   -- Git
-  use("tpope/vim-fugitive") -- integrates Git into Vim, allowing Git commands to be executed within the editor
-  use("Xuyuanp/nerdtree-git-plugin") -- indicating file states like staged, unstaged, modified, or deleted directly within the NERDTree file explorer
-  use("lewis6991/gitsigns.nvim") -- git decorations in editor
+  use("tpope/vim-fugitive")                                             -- integrates Git into Vim, allowing Git commands to be executed within the editor
+  use("Xuyuanp/nerdtree-git-plugin")                                    -- indicating file states like staged, unstaged, modified, or deleted directly within the NERDTree file explorer
+  use("lewis6991/gitsigns.nvim")                                        -- git decorations in editor
   use({ "sindrets/diffview.nvim", requires = "nvim-lua/plenary.nvim" }) -- Single tabpage interface for easily cycling through diffs for all modified files for any git rev.
+  use({
+    "NeogitOrg/neogit",
+    requires = {
+      "nvim-lua/plenary.nvim",         -- required
+      "sindrets/diffview.nvim",        -- optional - Diff integration
+      "nvim-telescope/telescope.nvim", -- optional
+    },
+    config = function()
+      require("neogit").setup({})
+    end,
+  })
 
   -- Highlights
   use({
-    "nvim-treesitter/nvim-treesitter", -- better syntax
+    "nvim-treesitter/nvim-treesitter", -- Better syntax
     run = function()
       require("nvim-treesitter.install").update({ with_sync = true })
     end,
   })
-  use("m-demare/hlargs.nvim") -- Highlight argument definitions and usages
-  use("neoclide/jsonc.vim") -- Syntax highlighting for CJSON
+  use({
+    "nvim-treesitter/nvim-treesitter-textobjects", -- Syntax aware text-objects, select, move, swap, and peek support
+    after = "nvim-treesitter",
+    requires = "nvim-treesitter/nvim-treesitter",
+  })
+  use("m-demare/hlargs.nvim")           -- Highlight argument definitions and usages
+  use("neoclide/jsonc.vim")             -- Syntax highlighting for CJSON
   use("ntpeters/vim-better-whitespace") -- Highlight all trailing whitespace characters
-  use("chrisbra/Colorizer") -- Color highlight in CSS
-  use("RRethy/vim-illuminate") -- Highlight other uses of word under cursor
+  use("chrisbra/Colorizer")             -- Color highlight in CSS
+  use("RRethy/vim-illuminate")          -- Highlight other uses of word under cursor
 
   -- Language Support
-  use("neovim/nvim-lspconfig") -- Collection of common configurations for the Nvim LSP client
-  use("williamboman/mason.nvim") -- Manage LSPs and DAPs
+  use("neovim/nvim-lspconfig")             -- Collection of common configurations for the Nvim LSP client
+  use("williamboman/mason.nvim")           -- Manage LSPs and DAPs
   use("williamboman/mason-lspconfig.nvim") -- closes gaps with mason
-  use("saecki/crates.nvim") -- Help managing crates dependencies
-  use("simrat39/rust-tools.nvim") -- rust-analyzer hints and more!
-  use("nvim-lua/plenary.nvim") -- Collection of Lua functions
-  use("dense-analysis/ale") -- Providing linting (syntax checking and semantic errors) in NeoVim 0.6.0+ and Vim 8.0+ while you edit your text files, and acts as a Vim Language Server Protocol client.
+  use("saecki/crates.nvim")                -- Help managing crates dependencies
+  use("simrat39/rust-tools.nvim")          -- rust-analyzer hints and more!
+  use("nvim-lua/plenary.nvim")             -- Collection of Lua functions
+  use("dense-analysis/ale")                -- Providing linting (syntax checking and semantic errors) in NeoVim 0.6.0+ and Vim 8.0+ while you edit your text files, and acts as a Vim Language Server Protocol client.
 
   -- Navigation
   use({
@@ -65,20 +90,20 @@ return require("packer").startup(function()
     requires = { { "nvim-lua/plenary.nvim" } },
   })
   use({ "nvim-telescope/telescope-fzf-native.nvim", run = "make" }) -- Make telescope faster
-  use("nvim-telescope/telescope-media-files.nvim") -- image files preview in telescope
-  use("BurntSushi/ripgrep") -- Telescope grep
-  use("hedyhli/outline.nvim") -- A sidebar with a tree-like outline of symbols from your code, powered by LSP
-  use("nvim-tree/nvim-tree.lua") -- Filesystem navigation
-  use({ "phaazon/hop.nvim", branch = "v2" }) -- Navitage to any word in the file
-  use("folke/trouble.nvim") -- Summarizes issues
+  use("nvim-telescope/telescope-media-files.nvim")                  -- image files preview in telescope
+  use("BurntSushi/ripgrep")                                         -- Telescope grep
+  use("hedyhli/outline.nvim")                                       -- A sidebar with a tree-like outline of symbols from your code, powered by LSP
+  use("nvim-tree/nvim-tree.lua")                                    -- Filesystem navigation
+  use({ "phaazon/hop.nvim", branch = "v2" })                        -- Navitage to any word in the file
+  use("folke/trouble.nvim")                                         -- Summarizes issues
 
   -- Performance
   use("nvim-neotest/nvim-nio") -- Asynchronous IO in Neovim
 
   -- UI
   use({ "rose-pine/neovim", as = "rose-pine" }) -- theme
-  use("goolord/alpha-nvim") -- fast and fully programmable greeter
-  use("j-hui/fidget.nvim") -- shows status of nvim-lsp progress
-  use("nvim-lualine/lualine.nvim") -- Statusline
-  use("nvim-lua/popup.nvim") -- Poup for telescope
+  use("goolord/alpha-nvim")                     -- fast and fully programmable greeter
+  use("j-hui/fidget.nvim")                      -- shows status of nvim-lsp progress
+  use("nvim-lualine/lualine.nvim")              -- Statusline
+  use("nvim-lua/popup.nvim")                    -- Poup for telescope
 end)
